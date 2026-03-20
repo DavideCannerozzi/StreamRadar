ee965692e6420479523fd9e1bab9ca07d999ffd7
Davide Cannerozzi <d.cannerozzi@gmail.com>
2026-03-20 14:13:43 +0100

    create table for movie list ui

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -2,19 +2,21 @@ import type { Result } from "../FileUploader/FileUploader.types";
 
 export default function MovieCard({ result }: { result: Result }) {
   return (
-    <div className="flex items-center justify-center gap-4 border-b border-gray-200 pb-4">
-      <span className="text-sm font-medium">{result.title}</span>
-      <span className="text-sm font-medium">{result.releaseDate}</span>
-      <div className="flex gap-8">
-        {result.platforms.map((platform) => (
-          <img
-            key={platform.provider_id}
-            src={`https://image.tmdb.org/t/p/original${platform.logo_path}`}
-            alt={platform.provider_name}
-            className="w-8 h-8 rounded-md"
-          />
-        ))}
-      </div>
-    </div>
+    <tr className="border-b border-gray-200">
+      <td className="text-sm font-medium py-4">{result.title}</td>
+      <td className="text-sm font-medium py-4">{result.releaseDate}</td>
+      <td className="py-4">
+        <div className="flex gap-8">
+          {result.platforms.map((platform) => (
+            <img
+              key={platform.provider_id}
+              src={`https://image.tmdb.org/t/p/original${platform.logo_path}`}
+              alt={platform.provider_name}
+              className="w-8 h-8 rounded-md"
+            />
+          ))}
+        </div>
+      </td>
+    </tr>
   );
 }
--- src/components/MovieList/MovieList.tsx
+++ src/components/MovieList/MovieList.tsx
@@ -7,10 +7,20 @@ interface MovieListProps {
 
 export default function MovieList({ results }: MovieListProps) {
   return (
-    <div className="flex flex-col gap-4 w-full">
-      {results.map((result, i) => (
-        <MovieCard key={i} result={result} />
-      ))}
-    </div>
+    <table className="table-auto border-red-400 w-full">
+      <thead>
+        <tr>
+          <th className="text-left pb-8">Title</th>
+          <th className="text-left pb-8">Year</th>
+          <th className="text-left pb-8">Platform</th>
+        </tr>
+      </thead>
+      <tbody>
+        {" "}
+        {results.map((result, i) => (
+          <MovieCard key={i} result={result} />
+        ))}
+      </tbody>
+    </table>
   );
 }
