642a5d7394cab6d8eb34daa7b46315063b231819
Davide Cannerozzi <d.cannerozzi@gmail.com>
2026-03-23 11:14:15 +0100

    add empty state message to movieList component

--- src/components/MovieList/MovieList.tsx
+++ src/components/MovieList/MovieList.tsx
@@ -7,20 +7,28 @@ interface MovieListProps {
 
 export default function MovieList({ results }: MovieListProps) {
   return (
-    <table className="table-auto border-red-400 w-full">
-      <thead>
-        <tr>
-          <th className="text-left pb-8">Title</th>
-          <th className="text-left pb-8">Year</th>
-          <th className="text-left pb-8">Platform</th>
-        </tr>
-      </thead>
-      <tbody>
-        {" "}
-        {results.map((result, i) => (
-          <MovieCard key={i} result={result} />
-        ))}
-      </tbody>
-    </table>
+    <>
+      {results.length === 0 ? (
+        <p className="text-gray-400">
+          Upload your IMDB watchlist and click Search to find available films
+        </p>
+      ) : (
+        <table className="table-auto border-red-400 w-full">
+          <thead>
+            <tr>
+              <th className="text-left pb-8">Title</th>
+              <th className="text-left pb-8">Year</th>
+              <th className="text-left pb-8">Platform</th>
+            </tr>
+          </thead>
+          <tbody>
+            {" "}
+            {results.map((result, i) => (
+              <MovieCard key={i} result={result} />
+            ))}
+          </tbody>
+        </table>
+      )}
+    </>
   );
 }
